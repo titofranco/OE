39a3be5657168c417ada265ea27ce7d3e8921834
titofranco <titofranco@gmail.com>
2010-11-05 10:35:57 -0500

    CRUD for Magazine, include file uploading for cover magazine

--- db/migrate/20101104153502_create_magazines.rb
+++ db/migrate/20101104153502_create_magazines.rb
@@ -1,8 +1,16 @@
 class CreateMagazines < ActiveRecord::Migration
   def self.up
     create_table :magazines do |t|
-
-      t.timestamps
+      t.string :title, :null => false
+      t.string :number_of_pages, :null => false
+      t.string :publisher
+      t.string :tags
+      t.date :release_date
+      t.timestamps :default => Time.now
+      t.string :cover_file_name
+      t.string :cover_content_type
+      t.integer :cover_file_size
+      t.datetime :cover_updated_at
     end
   end
 
