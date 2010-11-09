52c56f1eaf34dafbef63900575c38fb29af80533
titofranco <titofranco@gmail.com>
2010-11-09 17:35:41 -0500

    Change path where magazine cover images are stored

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -2,7 +2,7 @@ class Magazine < ActiveRecord::Base
   has_many :magazine_pages
   has_attached_file :cover , 
                     :styles => { :medium => "300x300", :thumb => "100x100"},
-                    :url => "/system/:class/:id/:style/:basename.:extension" , 
-                    :path => ":rails_root/public/system/:class/:id/:style/:basename.:extension"
+                    :url => "/system/:class/:id/:style_:basename.:extension" , 
+                    :path => ":rails_root/public/system/:class/:id/:style_:basename.:extension"
 
 end
