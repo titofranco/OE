f476e3cf96c6a2473efe79cf2aabb7ce6359bd24
titofranco <titofranco@gmail.com>
2010-11-08 20:22:06 -0500

    Change default path where images are stored

--- app/models/magazine_page.rb
+++ app/models/magazine_page.rb
@@ -2,6 +2,6 @@ class MagazinePage < ActiveRecord::Base
   belongs_to :magazine
   has_attached_file :image , 
                     :styles => { :medium => "300x300", :thumb => "100x100"},
-                    :url => "/system/magazine/:class/:style/:basename.:extension",
-                    :path => ":rails_root/public/system/magazine/:class/:style/:basename.:extension"                 
+                    :url => "/system/:class/:id/:style_:basename.:extension",
+                    :path => ":rails_root/public/system/:class/:id/:style_:basename.:extension"                 
 end
