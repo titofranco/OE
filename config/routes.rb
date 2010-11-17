c5eaad6659eb643a6dd28d3d54f663302cdb6923
titofranco <titofranco@gmail.com>
2010-11-17 15:13:08 -0500

    Revert "Revert "Revert "Include jrails as gem"""
    This reverts commit 0748282c08b64129804111de1765bd4296286309.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :magazines , :has_many => :pages, :shallow => true
-  map.resources :pages
+  map.resources :pages , :only => :index
 
   # The priority is based upon order of creation: first created -> highest priority.
 
