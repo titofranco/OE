e8f2abb907f183e859650d018b05d5c3afc511cc
titofranco <titofranco@gmail.com>
2010-11-17 15:02:08 -0500

    Include jrails as gem

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -1,23 +1,22 @@
-$(document).ready( function() {
-/*
-	$("#iframeCntnr").imBookFlip({
+jQuery(document).ready( function() {
+
+	jQuery("#iframeCntnr").imBookFlip({
 		page_class: 'imBookPage',
 		iframe: {src: 'book.html', book: 'myBook'}
-	});*/
+	});
 
-	$("#myBook").imBookFlip({
+	jQuery("#myBook").imBookFlip({
 		page_class: 'imBookPage'
 	});
 	
 	
-   $('#myPageFlip').jPageFlip({
-      width: 300,
-      height: 225,
-      top: 30,
-      left: 70
-    });
+  jQuery('#myPageFlip').jPageFlip({
+    width: 300,
+    height: 225,
+    top: 30,
+    left: 70
+  });
 	
 	
 });
-
 
