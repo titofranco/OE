26ae421fb2cc4137e2c9b0c0572551fc43e7f68e
titofranco <titofranco@gmail.com>
2010-11-22 15:42:16 -0500

    added model validations for magazine

--- app/models/magazine.rb
+++ app/models/magazine.rb
@@ -1,5 +1,9 @@
 class Magazine < ActiveRecord::Base
   has_many :pages
+  validates_attachment_presence :cover              
+  validates_attachment_content_type :cover, :content_type=>['image/jpeg', 'image/png', 'image/gif']
+  validates_presence_of :title, :number_of_pages
+  validates_uniqueness_of :title
   has_attached_file :cover , 
                     :styles => { :medium => "300x300", :thumb => "100x100"},
                     :url => "/system/:class/:id/:style_:basename.:extension" , 
