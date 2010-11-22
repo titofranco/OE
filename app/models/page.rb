1c3dc0a2809e1e15c7c84347eb6340f9e082fbfb
titofranco <titofranco@gmail.com>
2010-11-22 15:42:42 -0500

    added model validations for page

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,5 +1,10 @@
 class Page < ActiveRecord::Base
   belongs_to :magazine
+  validates_numericality_of :page_number
+  validates_presence_of :page_number
+  validates_uniqueness_of :page_number
+  validates_attachment_presence :image
+  validates_attachment_content_type :image, :content_type=>['image/jpeg', 'image/png', 'image/gif']
   has_attached_file :image , 
                     :styles => { :medium => "300x300", :thumb => "100x100"},
                     :url => "/system/:class/:id/:style_:basename.:extension",
