9c1f8b26e335bcd26fb845f31afadfb9ef54a5df
titofranco <titofranco@gmail.com>
2010-12-09 15:36:28 -0500

    play around with gem, no significant results

--- app/helpers/flickr_helper.rb
+++ app/helpers/flickr_helper.rb
@@ -2,10 +2,23 @@ module FlickrHelper
   def user_photos(user_id, photo_count = 12)
     flickr = Flickr.new(File.join(RAILS_ROOT, 'config', 'flickr.yml'))
     photos = flickr.photos.search(:user_id => user_id)
+    #onesinglephoto = flickr.photos.find_by_id('3016262284')
+    #puts "ONE SINGLE PHOTO #{onesinglephoto.inspect}"
     if photos.length < photo_count
        photo_count = photos.length
-    end   
+    end    
+   # photosets = flickr.photosets.get_list(:user_id => user_id)    
+   # puts "WHAT ARE PHOTOSETS #{photos.inspect}"
     photos = photos.values_at(0..(photo_count - 1)) 
+    return photos
+  end
+  
+  def one_photo 
+    flickr = Flickr.new(File.join(RAILS_ROOT, 'config', 'flickr.yml'))
+    onesinglephoto = flickr.photos.find_by_id('3016262284')
+    photosets = flickr.photosets.get_list(:user_id => '12864272@N02')
+    puts "PHOTOSET #{photosets.inspect}"
+    return onesinglephoto
   end
   
   def render_flickr_sidebar_widget(user_id, photo_count = 12, columns = 2)
@@ -16,4 +29,11 @@ module FlickrHelper
       render :partial => '/flickr/unavailable'
     end
   end
+  
+  def render_one_photo
+      onesinglephoto = one_photo
+      puts "QUE VA HOME #{onesinglephoto}"
+      render :partial => '/flickr/one_photo', :locals => {:photo => onesinglephoto}
+  end
+  
 end
