750967475d1f04c556681daacc10ef4ad1fae614
titofranco <titofranco@gmail.com>
2011-02-01 11:54:31 -0500

    Added API_KEY and SHARED_SECRET

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,11 +1,14 @@
 class AlbumsController < ApplicationController
-
+  
+  API_KEY = "891fffe180dca4fb47cca2b4a3bb6ec3"
+  SHARED_SECRET = "98352d68fc4db45a" 
+  
   before_filter :authenticate
 
   def authenticate
     flickr_cache = File.join(RAILS_ROOT, 'config', 'flickr.cache')
-    flickr = Flickr.new(flickr_cache,API_KEY,SHARED_SECRET)
-    user = flickr.people.findByUsername("titofranco")  
+    @flickr = Flickr.new(flickr_cache,API_KEY,SHARED_SECRET)
+    #user = flickr.people.findByUsername("titofranco")  
   end
 
 end
