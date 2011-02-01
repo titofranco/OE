a3c5ccdce9ee2a7c16e604dee2edf61e3eb3b8ef
titofranco <titofranco@gmail.com>
2011-02-01 11:27:56 -0500

    Fixed bugs for rflickr gem, works on rails 2.3.5

---
+++ vendor/plugins/rflickr/README.markdown
@@ -0,0 +1,23 @@
+# rFlickr
+
+To use this version of rFlickr, first you will need to install
+the RubyGem using the following command:
+
+    $ sudo gem install digitalpardoe-rflickr
+
+Then you'll need to get a Flickr API key as detailed here:
+
+[http://www.flickr.com/services/api/misc.api_keys.html](http://www.flickr.com/services/api/misc.api_keys.html)
+
+You'll need to authorize your application's API key from each
+Flickr user account which wants to use it.
+
+A short guide to get you started can be found here:
+
+[http://digitalpardoe.co.uk/blog/show/87](http://digitalpardoe.co.uk/blog/show/87)
+
+The guide is a little out of date in it's gem installation technique,
+use the instructions above instead.
+
+This project is still a work in progress, bear with me whilst it is
+set up.
---
+++ vendor/plugins/rflickr/lib/flickr.rb
@@ -0,0 +1,38 @@
+# rFlickr: A Ruby based Flickr API implementation.
+# Copyright (C) 2009, Alex Pardoe (digital:pardoe)
+#
+# Derrived from work by Trevor Schroeder, see here:
+# http://rubyforge.org/projects/rflickr/.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+
+require 'flickr/auth'
+require 'flickr/base'
+require 'flickr/blogs'
+require 'flickr/contacts'
+require 'flickr/favorites'
+require 'flickr/groups'
+require 'flickr/licenses'
+require 'flickr/notes'
+require 'flickr/people'
+require 'flickr/photos'
+require 'flickr/photosets'
+require 'flickr/pools'
+require 'flickr/reflection'
+require 'flickr/transform'
+require 'flickr/upload'
+require 'flickr/urls'
+require 'flickr/tags'
+require 'flickr/interestingness'
