aca872ef69e34a1385f5e42cfd0d0f74f7b3fe7a
titofranco <titofranco@gmail.com>
2011-02-01 11:48:48 -0500

    added filter authentication for flickr

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,2 +1,11 @@
 class AlbumsController < ApplicationController
+
+  before_filter :authenticate
+
+  def authenticate
+    flickr_cache = File.join(RAILS_ROOT, 'config', 'flickr.cache')
+    flickr = Flickr.new(flickr_cache,API_KEY,SHARED_SECRET)
+    user = flickr.people.findByUsername("titofranco")  
+  end
+
 end
