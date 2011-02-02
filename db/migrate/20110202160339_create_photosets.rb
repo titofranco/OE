d27c2b86bf74ba8be32b2ff37d59c970dd0c20da
titofranco <titofranco@gmail.com>
2011-02-02 18:14:50 -0500

    migration for photoset model

--- db/migrate/20110202160339_create_photosets.rb
+++ db/migrate/20110202160339_create_photosets.rb
@@ -1,9 +1,16 @@
 class CreatePhotosets < ActiveRecord::Migration
   def self.up
     create_table :photosets do |t|
-
-      t.timestamps
+      t.string :photoset_id , :limit => 25
+      t.integer :photo_count
+      t.string :title, :limit => 250
+      t.text :description
+      t.string :url, :limit => 250
+      t.timestamps :default => Time.now
     end
+    
+    add_index :photosets , :photoset_id
+    
   end
 
   def self.down
--- db/schema.rb
+++ db/schema.rb
@@ -9,20 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20101105153401) do
-
-  create_table "magazine_pages", :force => true do |t|
-    t.integer  "magazine_id",                                           :null => false
-    t.integer  "page_number",                                           :null => false
-    t.datetime "created_at",         :default => '2010-11-05 12:21:23'
-    t.datetime "updated_at",         :default => '2010-11-05 12:21:23'
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
-  end
-
-  add_index "magazine_pages", ["magazine_id"], :name => "fk_magazine_pages"
+ActiveRecord::Schema.define(:version => 20110202160339) do
 
   create_table "magazines", :force => true do |t|
     t.string   "title",                                                 :null => false
@@ -30,12 +17,37 @@ ActiveRecord::Schema.define(:version => 20101105153401) do
     t.string   "publisher"
     t.string   "tags"
     t.date     "release_date"
-    t.datetime "created_at",         :default => '2010-11-04 20:23:12'
-    t.datetime "updated_at",         :default => '2010-11-04 20:23:12'
+    t.datetime "created_at",         :default => '2011-02-02 15:34:37'
+    t.datetime "updated_at",         :default => '2011-02-02 15:34:37'
     t.string   "cover_file_name"
     t.string   "cover_content_type"
     t.integer  "cover_file_size"
     t.datetime "cover_updated_at"
   end
+
+  create_table "pages", :force => true do |t|
+    t.integer  "magazine_id",                                           :null => false
+    t.integer  "page_number",                                           :null => false
+    t.datetime "created_at",         :default => '2011-02-02 15:34:38'
+    t.datetime "updated_at",         :default => '2011-02-02 15:34:38'
+    t.string   "image_file_name"
+    t.string   "image_content_type"
+    t.integer  "image_file_size"
+    t.datetime "image_updated_at"
+  end
+
+  add_index "pages", ["magazine_id"], :name => "fk_pages"
+
+  create_table "photosets", :force => true do |t|
+    t.string   "photoset_id", :limit => 25
+    t.integer  "photo_count"
+    t.string   "title",       :limit => 250
+    t.text     "description"
+    t.string   "url",         :limit => 250
+    t.datetime "created_at",                 :default => '2011-02-02 18:10:53'
+    t.datetime "updated_at",                 :default => '2011-02-02 18:10:53'
+  end
+
+  add_index "photosets", ["photoset_id"], :name => "index_photosets_on_photoset_id"
 
 end
